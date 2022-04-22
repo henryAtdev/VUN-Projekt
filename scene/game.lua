06996f0c08ca5457a2b86fd9d28a4d072d4d957f
henryAtdev <98239456+henryAtdev@users.noreply.github.com>
2022-04-22 14:13:41 +0200

    Sounds entfernt

--- scene/game.lua
+++ scene/game.lua
@@ -23,17 +23,17 @@ function scene:create( event )
 	local sndDir = "scene/game/sfx/"
 	scene.sounds = {
 		thud = audio.loadSound( sndDir .. "thud.mp3" ),
-		sword = audio.loadSound( sndDir .. "sword.mp3" ),
-		squish = audio.loadSound( sndDir .. "squish.mp3" ),
-		slime = audio.loadSound( sndDir .. "slime.mp3" ),
-		wind = audio.loadSound( sndDir .. "loops/spacewind.mp3" ),
+		--sword = audio.loadSound( sndDir .. "sword.mp3" ),
+		--squish = audio.loadSound( sndDir .. "squish.mp3" ),
+		--slime = audio.loadSound( sndDir .. "slime.mp3" ),
+		--wind = audio.loadSound( sndDir .. "loops/spacewind.mp3" ),
 		door = audio.loadSound( sndDir .. "door.mp3" ),
 		hurt = {
-			audio.loadSound( sndDir .. "hurt1.mp3" ),
-			audio.loadSound( sndDir .. "hurt2.mp3" ),
+			--audio.loadSound( sndDir .. "hurt1.mp3" ),
+			--audio.loadSound( sndDir .. "hurt2.mp3" ),
 		},
-		hit = audio.loadSound( sndDir .. "hit.mp3" ),
-		coin = audio.loadSound( sndDir .. "coin.mp3" ),
+		--hit = audio.loadSound( sndDir .. "hit.mp3" ),
+		--coin = audio.loadSound( sndDir .. "coin.mp3" ),
 	}
 
 	-- Start physics before loading map
