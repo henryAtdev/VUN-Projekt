2b1f441a764d624855b7e79ca8d809422c4eef30
henryAtdev <98239456+henryAtdev@users.noreply.github.com>
2022-06-13 21:04:19 +0200

    first attempt to included json (currently not stable)

--- scene/refresh.lua
+++ scene/refresh.lua
@@ -3,19 +3,22 @@ local composer = require( "composer" )
 
 -- Variables local to scene
 local prevScene = composer.getSceneName( "previous" )
+local questionFilename = "scene/game/map/questions.json"
+local questionMapData = json.decodeFile( system.pathForFile( questionFilename, system.ResourceDirectory ) )
+var rndQuestion = questionMapData[ math.random( #questionMapData ) ]
 
 -- Create a new Composer scene
 local scene = composer.newScene()
 
 function scene:show( event )
 
-local myString1 = "Was ist die allgemeine Formel fuer F"
-local myString2 = "F= m*a"
-local myString3 = "F= m*v"
+local question = rndQuestion.question
+local answer1 = rndQuestion.answers[0]
+local answer2 = rndQuestion.answers[1]
 
-local myText1 = display.newText( myString1, display.contentCenterX, display.contentCenterY, 1000, 600, native.systemFont, 50 )
-local myText2 = display.newText( myString2, display.contentCenterX, display.contentCenterY, 1000,  300, native.systemFont, 30 )
-local myText3 = display.newText( myString3, display.contentCenterX, display.contentCenterY, 1000, 0, native.systemFont, 30 )
+local myText1 = display.newText( question, display.contentCenterX, display.contentCenterY, 1000, 600, native.systemFont, 50 )
+local myText2 = display.newText( answer1, display.contentCenterX, display.contentCenterY, 1000,  300, native.systemFont, 30 )
+local myText3 = display.newText( answer2, display.contentCenterX, display.contentCenterY, 1000, 0, native.systemFont, 30 )
 
 function test(event)
 	composer.gotoScene( "scene.menu")
