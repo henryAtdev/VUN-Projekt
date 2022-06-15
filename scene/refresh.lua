89793492986c5a5a0bc0c413935257492952dcdb
Andreas Bib <bibatdevelopment@gmail.com>
2022-06-16 00:26:36 +0200

    Added json decoder

--- scene/refresh.lua
+++ scene/refresh.lua
@@ -1,11 +1,51 @@
 -- Include modules/libraries
+-- package.path = package.path .. ";../lib/?.lua"
+-- local lunajson = require("lunajson")
+
 local composer = require( "composer" )
+local json = require( "json" )  -- Include the Corona JSON library
+
+
+-- https://docs.coronalabs.com/tutorial/data/jsonSaveLoad/index.html
+function readJSONFromFile( filename, location )
+ 
+    local loc = location
+    if not location then
+        loc = system.ResourceDirectory
+    end
+ 
+    -- Path for the file to read
+    local path = system.pathForFile( filename, loc )
+ 
+    -- Open the file handle
+    local file, errorString = io.open( path, "r" )
+ 
+    if not file then
+        -- Error occurred; output the cause
+        print( "File error: " .. errorString )
+    else
+        -- Read data from file
+        local contents = file:read( "*a" )
+        -- Decode JSON data into Lua table
+        local t = json.decode( contents )
+        -- Close the file handle
+        io.close( file )
+        -- Return table
+        return t
+    end
+end
+
+
 
 -- Variables local to scene
 local prevScene = composer.getSceneName( "previous" )
-local questionFilename = "scene/game/map/questions.json"
-local questionMapData = json.decodeFile( system.pathForFile( questionFilename, system.ResourceDirectory ) )
-var rndQuestion = questionMapData[ math.random( #questionMapData ) ]
+
+
+local questionMapData = readJSONFromFile( "config/questions.json", system.ResourceDirectory )
+
+
+local rndQuestion = questionMapData[0]
+print( json.encode( rndQuestion ) ) 
 
 -- Create a new Composer scene
 local scene = composer.newScene()
@@ -13,12 +53,12 @@ local scene = composer.newScene()
 function scene:show( event )
 
 local question = rndQuestion.question
-local answer1 = rndQuestion.answers[0]
-local answer2 = rndQuestion.answers[1]
+local answer1 = rndQuestion.answers[0].value
+local answer2 = rndQuestion.answers[1].value
 
-local myText1 = display.newText( question, display.contentCenterX, display.contentCenterY, 1000, 600, native.systemFont, 50 )
-local myText2 = display.newText( answer1, display.contentCenterX, display.contentCenterY, 1000,  300, native.systemFont, 30 )
-local myText3 = display.newText( answer2, display.contentCenterX, display.contentCenterY, 1000, 0, native.systemFont, 30 )
+local myText1 = display.newText( json.encode( question ), display.contentCenterX, display.contentCenterY, 1000, 600, native.systemFont, 50 )
+local myText2 = display.newText( json.encode( answer1 ), display.contentCenterX, display.contentCenterY, 1000,  300, native.systemFont, 30 )
+local myText3 = display.newText( json.encode(answer2 ), display.contentCenterX, display.contentCenterY, 1000, 0, native.systemFont, 30 )
 
 function test(event)
 	composer.gotoScene( "scene.menu")
